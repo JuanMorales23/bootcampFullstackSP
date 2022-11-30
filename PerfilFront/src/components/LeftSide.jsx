01248357ebcc316b38f8af0b360f3e1bbc8e1195
JuanMorales23 <juanmr2311@gmail.com>
2022-11-30 01:47:00 -0500

    Se modifican textos y identaciones con las listas

--- PerfilFront/src/components/LeftSide.jsx
+++ PerfilFront/src/components/LeftSide.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import gmail from '../assets/img/icons8-gmail-48.png';
-import phone from '../assets/img/icons8-whatsapp-48.png';
-import facebook from '../assets/img/icons8-facebook-48.png';
-import linkedin from '../assets/img/icons8-linkedin-48.png';
-import github from '../assets/img/icons8-github-48.png';
+import gmail from "../assets/img/icons8-gmail-48.png";
+import phone from "../assets/img/icons8-whatsapp-48.png";
+import facebook from "../assets/img/icons8-facebook-48.png";
+import linkedin from "../assets/img/icons8-linkedin-48.png";
+import github from "../assets/img/icons8-github-48.png";
 
 const LeftSide = () => {
   return (
@@ -14,24 +14,86 @@ const LeftSide = () => {
           <div className="table-responsive">
             <table className="table">
               <tr>
-              <td className="tdIcon py-0"><img src={phone} alt="Telefono" className="imgIcon"/></td>
-                <td className="py-0"><p>+57 310-530-0721</p></td>
+                <td className="tdIcon py-0">
+                  <a href="https://api.whatsapp.com/send?phone=573105300721">
+                    <img src={phone} alt="Whatsapp" className="imgIcon" />
+                  </a>
+                </td>
+                <td className="">
+                  <p>
+                    <a href="https://api.whatsapp.com/send?phone=573105300721">
+                      +57 310-530-0721
+                    </a>
+                  </p>
+                </td>
               </tr>
               <tr>
-                <td className="tdIcon py-0"><a href="mailto:juanmr2311@gmail.com" target="_blank"><img src={gmail} alt="Correo" className="imgIcon"/></a></td>
-                <td className="py-0"><p><a href="mailto:juanmr2311@gmail.com" target="_blank">juanmr2311@gmail.com</a></p></td>
+                <td className="tdIcon py-0">
+                  <a href="mailto:juanmr2311@gmail.com" target="_blank">
+                    <img src={gmail} alt="Correo" className="imgIcon" />
+                  </a>
+                </td>
+                <td className="">
+                  <p>
+                    <a href="mailto:juanmr2311@gmail.com" target="_blank">
+                      juanmr2311@gmail.com
+                    </a>
+                  </p>
+                </td>
               </tr>
               <tr>
-              <td className="tdIcon py-0"><a href="www.facebook.com" target="_blank"><img src={facebook} alt="Facebook" className="imgIcon"/></a></td>
-                <td className=""><p><a href="www.facebook.com" target="_blank">Juan Esteban Morales Rios</a></p></td>
+                <td className="tdIcon py-0">
+                  <a
+                    href="https://www.facebook.com/juan.moralesrios.23"
+                    target="_blank"
+                  >
+                    <img src={facebook} alt="Facebook" className="imgIcon" />
+                  </a>
+                </td>
+                <td className="">
+                  <p>
+                    <a
+                      href="https://www.facebook.com/juan.moralesrios.23"
+                      target="_blank"
+                    >
+                      Juan Esteban Morales Rios
+                    </a>
+                  </p>
+                </td>
               </tr>
               <tr>
-              <td className="tdIcon py-0"><a href="https://www.linkedin.com/in/juan-esteban-morales-rios-52614b217/" target="_blank"><img src={linkedin} alt="Correo" className="imgIcon"/></a></td>
-                <td className="py-0"><p><a href="https://www.linkedin.com/in/juan-esteban-morales-rios-52614b217/" target="_blank">https://www.linkedin.com/in/juan-esteban-morales-rios-52614b217/</a></p></td>
+                <td className="tdIcon py-0">
+                  <a
+                    href="https://www.linkedin.com/in/juan-esteban-morales-rios-52614b217/"
+                    target="_blank"
+                  >
+                    <img src={linkedin} alt="Correo" className="imgIcon" />
+                  </a>
+                </td>
+                <td className="py-0">
+                  <p>
+                    <a
+                      href="https://www.linkedin.com/in/juan-esteban-morales-rios-52614b217/"
+                      target="_blank"
+                    >
+                      https://www.linkedin.com/in/juan-esteban-morales-rios-52614b217/
+                    </a>
+                  </p>
+                </td>
               </tr>
               <tr>
-              <td className="tdIcon py-0"><a href="https://github.com/JuanMorales23" target="_blank"><img src={github} alt="Correo" className="imgIcon"/></a></td>
-                <td className=""><p><a href="https://github.com/JuanMorales23" target="_blank">JuanMorales23</a></p></td>
+                <td className="tdIcon py-0">
+                  <a href="https://github.com/JuanMorales23" target="_blank">
+                    <img src={github} alt="Correo" className="imgIcon" />
+                  </a>
+                </td>
+                <td className="">
+                  <p>
+                    <a href="https://github.com/JuanMorales23" target="_blank">
+                      JuanMorales23
+                    </a>
+                  </p>
+                </td>
               </tr>
             </table>
           </div>
@@ -52,12 +114,14 @@ const LeftSide = () => {
       </div>
       <div className="row">
         <div className="col">
-          <h3>Habilidades</h3>
+          <h3>Competencias</h3>
           <ul>
             <li>
-              Conocimiento en lenguajes de programación orientados a objetos
+              <p>Trabajo y apoyo en equipo</p>
             </li>
-            <li>Trabajo y apoyo junto al equipo.</li>
+            <li><p>Respetuoso y responsable</p></li>
+            <li><p>Abierto al aprendizaje y retroalimentaciones</p></li>
+            <li><p>Comprometido</p></li>
           </ul>
         </div>
       </div>
@@ -66,7 +130,9 @@ const LeftSide = () => {
           <h3>Habilidades Técnicas</h3>
           <ul>
             <li className="dotHide">
-              <p>Java</p>
+              <p>
+                <b>Java</b>
+              </p>
               <div className="progress">
                 <div
                   className="progress-bar bg-success"
@@ -75,12 +141,13 @@ const LeftSide = () => {
                   aria-valuenow="75"
                   aria-valuemin="0"
                   aria-valuemax="100"
-                >
-                </div>
+                ></div>
               </div>
             </li>
             <li className="dotHide">
-              <p>PHP</p>
+              <p>
+                <b>Php</b>
+              </p>
               <div className="progress">
                 <div
                   className="progress-bar bg-success"
@@ -89,11 +156,114 @@ const LeftSide = () => {
                   aria-valuenow="75"
                   aria-valuemin="0"
                   aria-valuemax="100"
-                >
-                </div>
+                ></div>
               </div>
             </li>
-            <li className="dotHide"></li>
+            <li className="dotHide">
+              <p>
+                <b>Python</b>
+              </p>
+              <div className="progress">
+                <div
+                  className="progress-bar bg-success"
+                  role="progressbar"
+                  style={{ width: "50%" }}
+                  aria-valuenow="50"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </li>
+            <li className="dotHide">
+              <p>
+                <b>Html</b>
+              </p>
+              <div className="progress">
+                <div
+                  className="progress-bar bg-success"
+                  role="progressbar"
+                  style={{ width: "75%" }}
+                  aria-valuenow="75"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </li>
+            <li className="dotHide">
+              <p>
+                <b>Css</b>
+              </p>
+              <div className="progress">
+                <div
+                  className="progress-bar bg-success"
+                  role="progressbar"
+                  style={{ width: "50%" }}
+                  aria-valuenow="50"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </li>
+            <li className="dotHide">
+              <p>
+                <b>JavaScript</b>
+              </p>
+              <div className="progress">
+                <div
+                  className="progress-bar bg-success"
+                  role="progressbar"
+                  style={{ width: "75%" }}
+                  aria-valuenow="75"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </li>
+            <li className="dotHide">
+              <p>
+                <b>Git/GitHub</b>
+              </p>
+              <div className="progress">
+                <div
+                  className="progress-bar bg-success"
+                  role="progressbar"
+                  style={{ width: "75%" }}
+                  aria-valuenow="75"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </li>
+            <li className="dotHide">
+              <p>
+                <b>MySQL/Oracle Database</b>
+              </p>
+              <div className="progress">
+                <div
+                  className="progress-bar bg-success"
+                  role="progressbar"
+                  style={{ width: "50%" }}
+                  aria-valuenow="50"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </li>
+            <li className="dotHide">
+              <p>
+                <b>MongoDB</b>
+              </p>
+              <div className="progress">
+                <div
+                  className="progress-bar bg-success"
+                  role="progressbar"
+                  style={{ width: "50%" }}
+                  aria-valuenow="50"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                ></div>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
--- PerfilFront/src/components/RightSide.jsx
+++ PerfilFront/src/components/RightSide.jsx
@@ -17,6 +17,7 @@ const RightSide = () => {
             matemática adquiridas en el transcurso de mi formación
             universitaria.
           </p>
+          <br />
         </div>
       </div>
       <div className="row">
@@ -24,33 +25,41 @@ const RightSide = () => {
           <h3>Experiencia Laboral</h3>
           <ul>
             <li>
-              <ul>
-                <li className='dotHide'><b><p>Libre Gestión</p></b></li>
-                <li className='dotHide'>Desarollador</li>
-                <li className='dotHide'>Contacto: </li>
-                <li className='dotHide'>Octumbre 2022 - Actualmente</li>
-              </ul>
+              <h5>Libre Gestión</h5>
             </li>
+              <li className="dotHide">
+                <p>Desarollador</p>
+              </li>
+              <li className="dotHide">
+                <p>Contacto: </p>
+              </li>
+              <li className="dotHide">
+                <p>Octumbre 2022 - Actualmente</p>
+              </li>
+           
+            <br />
             <li>
-              <ul>
-                <li className='dotHide'>
-                  Politécnico Colombiano Jaime Isaza Cadavid (Coordinación de
-                  Nuevas Tecnologías Educativas)
-                </li>
-                <li className='dotHide'>Practicante</li>
-                <li className='dotHide'>Contacto: 311-231-9563</li>
-                <li className='dotHide'>Marzo 2022 - Julio 2022</li>
-              </ul>
+              {" "}
+              <h5>
+                Politécnico Colombiano Jaime Isaza Cadavid (Coordinación de
+                Nuevas Tecnologías Educativas){" "}
+              </h5>
             </li>
+
+              <li className="dotHide">Practicante</li>
+              <li className="dotHide">Contacto: 311-231-9563</li>
+              <li className="dotHide">Marzo 2022 - Julio 2022</li>
+            <br />
             <li>
-              <ul>
-                <li className='dotHide'>Giovanni Pia e Hijos S.A</li>
-                <li className='dotHide'>Operario</li>
-                <li className='dotHide'>Contacto: 384-33-00</li>
-                <li className='dotHide'>Noviembre 2015 - Diciembre 2015</li>
-              </ul>
+              <h5>Giovanni Pia e Hijos S.A</h5>
             </li>
+
+              <li className="dotHide">Operario</li>
+              <li className="dotHide">Contacto: 384-33-00</li>
+              <li className="dotHide">Noviembre 2015 - Diciembre 2015</li>
+
           </ul>
+          <br />
         </div>
       </div>
       <div className="row">
@@ -58,26 +67,22 @@ const RightSide = () => {
           <h3>Educación</h3>
           <ul>
             <li>
-              <ul>
-                <li className='dotHide'>Politécnico Colombiano Jaime Isaza Cadavid</li>
-                <li className='dotHide'>Ingeniería Informática</li>
-                <li className='dotHide'>Enero 2016 - Julio 2022</li>
-              </ul>
+              <h5>Politécnico Colombiano Jaime Isaza Cadavid</h5>
             </li>
+            <li className="dotHide">Ingeniería Informática</li>
+            <li className="dotHide">Enero 2016 - Julio 2022</li>
+            <br />
             <li>
-              <ul>
-                <li className='dotHide'>Institución Educativa Federico Ángel</li>
-                <li className='dotHide'>Bachiller Académico</li>
-                <li className='dotHide'>Febrero 2013 - Noviembre 2014</li>
-              </ul>
+              <h5>Institución Educativa Federico Ángel</h5>
             </li>
+            <li className="dotHide">Bachiller Académico</li>
+            <li className="dotHide">Febrero 2013 - Noviembre 2014</li>
+            <br />
             <li>
-              <ul>
-                <li className='dotHide'>Colegio Ciro Mendía</li>
-                <li className='dotHide'>Bachiller Académico</li>
-                <li className='dotHide'>Febrero 2009 - Noviembre 2012</li>
-              </ul>
+              <h5>Colegio Ciro Mendía</h5>
             </li>
+            <li className="dotHide">Bachiller Académico</li>
+            <li className="dotHide">Febrero 2009 - Noviembre 2012</li>
           </ul>
         </div>
       </div>
